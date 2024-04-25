9fe7d283996d9ba356f10efb55a3b178ad17a7d8
Francilândio <francilandiolima@alu.ufc.br>
2024-04-25 09:56:46 -0300

    Entrega do código final

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-let nome = "felipe";
-console.log(nome);
+let heroi = ["Pikachu", 5000];//vetor que armazena informação de nome e XP de um herói
+let nivel = "";//variável para indicar o nível do herói
+
+//laço de classificação do herói com base em seu XP
+if(heroi[1] < 1000)
+    {nivel = "Ferro";}
+else if(heroi[1] >= 1000 && heroi[1] < 2000)
+    {nivel = "Bronze";}
+else if(heroi[1] >= 2000 && heroi[1] < 5000)
+    {nivel = "Prata";}
+else if(heroi[1] >= 5000 && heroi[1] < 7000)
+    {nivel = "Ouro";}
+else if(heroi[1] >= 7000 && heroi[1] < 8000)
+    {nivel = "Platina";}
+else if(heroi[1] >= 8000 && heroi[1] < 9000)
+    {nivel = "Ascendente";}
+else if(heroi[1] >= 9000 && heroi[1] < 10000)
+    {nivel = "Imortal";}
+else
+    {nivel = "Radiante";}
+
+console.log("O Herói de nome "+heroi[0]+" está no nível de "+nivel);
